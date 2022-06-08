44f4a6a853e628d4ce1423539d15dadbad34902e
tbaiguzhinov <tim.baiguzhinov@gmail.com>
2022-06-08 12:45:33 +0600

    Add paginator

--- render_website.py
+++ render_website.py
@@ -1,5 +1,6 @@
 import json
 import os
+import math
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
@@ -17,8 +18,11 @@ def create_index():
     index_page_folder = "pages"
     os.makedirs(index_page_folder, exist_ok=True)
     book_pages = list(chunked(books, 20))
+    pages_quantity = math.ceil(len(books)/20)
     for num, book_page in enumerate(book_pages, 1):
         rendered_page = template.render(
+            current_page=num,
+            pages_quantity=pages_quantity,
             books=list(chunked(book_page, 2)),
         )
         with open(
